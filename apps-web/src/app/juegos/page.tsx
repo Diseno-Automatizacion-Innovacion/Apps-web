13243164f37301cc14201de0e447d3f69ec768d9
ZaCKz37 <camamovilcubo@gmail.com>
2023-10-30 18:32:31 +0100

    Update page.tsx

--- apps-web/src/app/juegos/page.tsx
+++ apps-web/src/app/juegos/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Home() {
 
     const [data, setData] = useState([] as any)
-
+    const router = useRouter()
     useEffect(() => {
         async function getData() {
             const data = await (await fetch("/api/juegos")).json()
@@ -15,13 +16,18 @@ export default function Home() {
         getData()
     }, [])
 
-    return (
-        <>
-            {
-                data.map((el: any) => {
-                    return <a className='text-white' href={`/juegos/${el.id}`} id={el.id} key={el.id}>{el.attributes.Titulo}</a>
-                })
-            }
-        </>
-    )
+    if (!router.query.slug) {
+        return (
+            <>
+                {
+                    data.map((el: any) => {
+                        return <a className='text-white' href={`/juegos/${el.id}`} id={el.id} key={el.id}>{el.attributes.Titulo}</a>
+                    })
+                }
+            </>
+        )
+    }
+    else {
+
+    }
 }
