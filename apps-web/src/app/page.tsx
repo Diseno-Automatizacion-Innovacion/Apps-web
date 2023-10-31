a4d102ecca91e58a8d0d5eb1236f13d37d039964
Ruben Garcalia <rubeneitor2@hotmail.com>
2023-10-31 02:41:46 +0100

    El package lock dando por culo y las " " cambiadas para que el linter no de por culo con que se puede renderizar mal la wea

--- apps-web/src/app/page.tsx
+++ apps-web/src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           <Image className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://strapi.garcalia.com/uploads/download_815f3c00e7.jpg" alt="Minecraft" width={1920} height={1080} />
           <div className="flex flex-col justify-between p-4 leading-normal">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Minecraft</h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Ultimos mods para "Minecraft"</p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Ultimos mods para &quot;Minecraft&quot;</p>
           </div>
         </a>
       </div>
