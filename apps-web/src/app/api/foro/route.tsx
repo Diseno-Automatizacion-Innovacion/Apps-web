6de4c49197a260b23c142e17d571163e16ef8823
r1d <camamovilcubo@gmail.com>
2023-12-01 19:39:44 +0100

    ñl

--- apps-web/src/app/api/foro/route.tsx
+++ apps-web/src/app/api/foro/route.tsx
@@ -1,6 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 
+export async function GET(req: NextRequest) {
+
+    let temas = await (await fetch(`https://wpbackend.garcalia.com/index.php/wp-json/rub-forum/v1/categories`)).json()
+
+    return NextResponse.json({
+        temas
+    })
+}
 
 export async function POST(req: NextRequest) {
     let data = await req.json()
--- apps-web/src/app/components/MainComponents/tema.tsx
+++ apps-web/src/app/components/MainComponents/tema.tsx
@@ -1,7 +1,4 @@
-// import { useRouter } from "next/navigation"
-
 export default function Tema(props: any) {
-    // const router = useRouter()
     return (
         <a href={`/foro/${props.id}`}>
             <div id="titulo">{props.title}</div>
