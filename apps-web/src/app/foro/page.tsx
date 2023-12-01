a9f3ae7906fdaaf799742f53c912106f5b6ecb9f
Ruben Garcalia <rubeneitor2@hotmail.com>
2023-12-01 19:29:17 +0100

    a

--- apps-web/src/app/foro/page.tsx
+++ apps-web/src/app/foro/page.tsx
@@ -2,11 +2,14 @@
 
 import { useEffect, useState } from "react"
 
-import Nav from "../components/Navigation"
-import IndexTitle from "../components/IndexTitle"
-import Tema from "../components/MainComponents/tema"
+import Nav from "@/app/components/Navigation"
+import IndexTitle from "@/app/components/IndexTitle"
+import Tema from "@/app/components/MainComponents/Tema"
 
 export default function Forum() {
+
+    const [categories, setCategories] = useState([] as any)
+
     useEffect(() => {
         async function getTema() {
             const token = localStorage.getItem("token")
@@ -15,15 +18,19 @@ export default function Forum() {
 
                 })).json()
             }
-            else {
-
-            }
         }
     }, [])
     return (
         <>
             <Nav></Nav>
             <IndexTitle title="Bienvenidos al Foro" secondTitle="Modink"></IndexTitle>
+            <div id="temas">
+                {
+                    categories?.map((category: any, i: number) => {
+                        return <Tema key={category.category_id} id={category.category_id} titulo={category?.category_name} descripcion={category?.category_description}></Tema>
+                    })
+                }
+            </div>
         </>
     )
 }
