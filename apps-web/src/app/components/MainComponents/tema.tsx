6eac99978ddf93ab95cf48d8ffd93d65e5cf7e5f
r1d <camamovilcubo@gmail.com>
2023-12-01 19:32:02 +0100

    Update tema.tsx

--- apps-web/src/app/components/MainComponents/tema.tsx
+++
@@ -1,8 +0,0 @@
-export default function Tema(props: any) {
-    return (
-        <a href={`/foro/${props.id}`}>
-            <div id="titulo">{props.title}</div>
-            <div id="description">{props.descripcion}</div>
-        </a>
-    )
-}
